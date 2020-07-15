59b2549202f39f60f1ed4a47a1aaa334a3f3a5e5
Cursos-aap <eduardo.jimenez.mir@gmail.com>
2020-07-15 11:23:22 -0500

    More

--- src/opcional2/Exercises.java
+++ src/opcional2/Exercises.java
@@ -6,10 +6,74 @@ public class Exercises {
 
     public static void main(String[] args){
 
-        //OptionalClass3.sumDigitsUntilLength1();
-        OptionalClass3.fibonacciSeries();
+        Scanner scanner = new Scanner(System.in);
 
-        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa un palídromo");
+
+        String palindrome = scanner.nextLine()
+                .replaceAll(" +", "");
+
+        StringBuilder stringBuilder = new StringBuilder(palindrome);
+
+        String reversed_string = stringBuilder
+                .reverse()
+                .toString()
+                .replaceAll(" +", "");
+
+        boolean condition = palindrome.equalsIgnoreCase(reversed_string);
+
+        if(condition)
+            System.out.println("Es palíndromo");
+        else
+            System.out.println("No es palíndromo");
+
+    }
+
+    public static void askNumberForMatrix(){
+        System.out.println("Mis números UwU\n");
+
+        //Read number from stdin
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa un número natural");
+        String number = scanner.nextLine();
+
+        while (!isNaturalNumber(number)){
+
+            System.out.println("Entrada inválida, ingresa un número natural: ");
+            number = scanner.nextLine();
+
+        }
+
+        printNMatrix(Integer.parseInt(number));
+
+    }
+
+    public static boolean isNaturalNumber(String number){
+        try {
+            if(Integer.parseInt(number) >= 0)
+                return true;
+        }catch(NumberFormatException e){
+            //e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void printNMatrix (int n){
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n; j++)
+                stringBuilder.append(j + i * n).append(' ');
+            stringBuilder.append('\n');
+        }
+
+        System.out.println(stringBuilder);
+    }
+
+    public static void readTriangleData(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Ingresa un número natural menor a " + Byte.MAX_VALUE);
 
         if(scanner.hasNextByte()) {
@@ -25,9 +89,6 @@ public class Exercises {
         }
         else
             System.out.println("No es un número");
-
-
-         */
     }
 
     public static void printTriangle(byte height){
